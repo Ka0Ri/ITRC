d00bc6873b487cd9cc0bc2cb7f13f9174cd8a066
vu <dtvu1707@gmail.com>
2021-12-12 04:45:39 +0000

    add swinIR

--- dataload.py
+++ dataload.py
@@ -1,27 +1,29 @@
 import yaml
 from glob import glob
+import time
 
-train_images = glob('dataset/hardhat/train/images/*jpg')
-test_images = glob('dataset/hardhat/test/images/*jpg')
+train_images = glob('dataset/Mask/train/images/*jpg')
+test_images = glob('dataset/Mask/valid/images/*jpg')
 
 print(f'total number of train images: {len(train_images)}')
 print(f'total number of train images: {len(test_images)}')
 
-with open('dataset/hardhat/train.txt', 'w') as f:
+with open('dataset/Mask/train.txt', 'w') as f:
   f.write('\n'.join(train_images) + '\n')
 
-with open('dataset/hardhat/test.txt', 'w') as f:
+with open('dataset/Mask/test.txt', 'w') as f:
   f.write('\n'.join(test_images) + '\n')
 
-with open('dataset/hardhat/data.yaml', 'r') as f:
+with open('dataset/Mask/data.yaml', 'r') as f:
   data = yaml.load(f, Loader=yaml.FullLoader)
 
 print(data)
+time.sleep(10)
 
-data['train'] = 'dataset/hardhat/train.txt'
-data['val'] = 'dataset/hardhat/test.txt'
+data['train'] = 'dataset/Mask/train.txt'
+data['val'] = 'dataset/Mask/test.txt'
 
-with open('dataset/hardhat/data.yaml', 'w') as f:
+with open('dataset/Mask/data.yaml', 'w') as f:
   yaml.dump(data, f)
 
 print(data)
